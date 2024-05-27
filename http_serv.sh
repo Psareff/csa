f4c5f83e94007e42d2580824115ae19cc870dc62
Ilya Psaryov <ilyapsareff@gmail.com>
2024-05-27 11:04:40 +0700

    First try of systemd

--- http_serv.sh
+++ http_serv.sh
@@ -1,6 +1,7 @@
 mkdir -p ./deb_http_server
-mkdir -p ./deb_http_server/bin
+mkdir -p ./deb_http_server/usr/local/bin
 mkdir -p ./deb_http_server/DEBIAN
+mkdir -p ./deb_http_server/etc/systemd/system
 touch  ./deb_http_server/DEBIAN/control
 
 cat << EOF > ./deb_http_server/DEBIAN/control
@@ -10,6 +11,14 @@ Architecture: amd64
 Maintainer: Psareff
 EOF
 
-cp ./build/HTTPServer ./deb_http_server/bin/
+cp ./build/HTTPServer ./deb_http_server/usr/local/bin/
+
+cp ./build/HTTPServer.service ./deb_http_server/etc/systemd/system/
+
+cp ./postinst ./deb_http_server/DEBIAN/
+cp ./prerm ./deb_http_server/DEBIAN/
+
+chmod +x ./deb_http_server/DEBIAN/postinst ./deb_http_server/DEBIAN/prerm
 
 dpkg-deb --build ./deb_http_server/ HTTPServer.deb
+sudo dpkg -i HTTPServer.deb
